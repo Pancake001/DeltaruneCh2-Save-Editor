718ea1442183b39f056302454e4c65f249d128ca
Pancake001 <pancakesoftyt@gmail.com>
2021-09-28 15:11:11 -0400

    Dark World Kris Box asdbnjffdbn

--- DeltaruneCh2 Save Editor/Form1.vb
+++ DeltaruneCh2 Save Editor/Form1.vb
@@ -21,20 +21,23 @@ Public Class Form1
             ListBox1.Items.AddRange(lines)
 
             '' Checks if Lightworld kris is on.
-            If ListBox1.Items(16) = 1 Then
-                darkkrisbox.Checked = False
-            End If
+            ''If ListBox1.Items(16) = 1 Then
+            ''darkkrisbox.Checked = True
+            ''Else
+            ''  darkkrisbox.Checked = False
+            ''End If
 
-            If ListBox1.Items(16) = 0 Then
-                darkkrisbox.Checked = True
-            End If
 
 
         End If
     End Sub
 
-    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
-
-
+    Private Sub darkkrisbox_CheckedChanged(sender As Object, e As EventArgs) Handles darkkrisbox.CheckedChanged
+        If darkkrisbox.Checked = True Then
+            ListBox1.Items(16) = 1 ''Changes Kris to be in Dark World form.
+        End If
+        If darkkrisbox.Checked = False Then
+            ListBox1.Items(16) = 0 ''Changes Kris to be in Light World form.
+        End If
     End Sub
 End Class
