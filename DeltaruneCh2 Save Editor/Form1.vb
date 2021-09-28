69d3e285aa03bf41a43d2b180f75bacaa6a2707c
Pancake001 <pancakesoftyt@gmail.com>
2021-09-28 12:24:51 -0400

    Paib

--- DeltaruneCh2 Save Editor/Form1.vb
+++ DeltaruneCh2 Save Editor/Form1.vb
@@ -10,23 +10,16 @@ Public Class Form1
 
         Dim appdataPath As String = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData)
         Dim Delfol As String = appdataPath + "\DELTARUNE"
-        OpenFileDialog1.Filter = "Deltarune Chapter 2 Save File |*2_0; *2_1; *2_2; *2_9;"
+        OpenFileDialog1.Filter = "Deltarune Chapter 2 Save File |*2_0; *2_1; *2_2; *2_9; | Text Files |*.txt"
         OpenFileDialog1.InitialDirectory = Delfol
         OpenFileDialog1.Title = "Please Open Deltarune Save File"
         OpenFileDialog1.FileName = "Filech2_0"
         If OpenFileDialog1.ShowDialog = DialogResult.OK Then
+            Title.Text = DialogResult.OK
             ListBox1.Items.Add(DialogResult)
 
 
         End If
-
-    End Sub
-
-
-
-
-
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
 End Class
